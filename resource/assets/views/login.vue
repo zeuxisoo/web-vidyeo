c99527662457f81cd4d8c48a9954332efc287074
Zeuxis Lo <seekstudio@gmail.com>
2015-10-08 18:12:42 +0800

    add layout for login and sign up page

--- resource/assets/views/login.vue
+++ resource/assets/views/login.vue
@@ -1,5 +1,22 @@
 <template>
-Login
+    <form>
+        <h3>Login</h3>
+        <hr>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="form-group">
+                    <label for="account">Account</label>
+                    <input type="text" class="form-control" id="account" placeholder="Username or Email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+            </div>
+        </div>
+        <hr>
+        <button type="submit" class="btn btn-default">Login</button>
+    </form>
 </template>
 
 <script lang="es6">
