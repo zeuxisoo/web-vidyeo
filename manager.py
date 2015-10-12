7bc98a6ec96b456d6724a483f6d406dec2e4d1c3
Zeuxis Lo <seekstudio@gmail.com>
2015-10-12 18:30:16 +0800

    add ssl server commands

--- manager.py
+++ manager.py
@@ -3,6 +3,7 @@ from vidyeo.app import create_app
 
 manager = Manager(create_app())
 manager.add_command('runserver', Server())
+manager.add_command('runsslserver', Server(ssl_context='adhoc'))
 
 if __name__ == "__main__":
     manager.run()
